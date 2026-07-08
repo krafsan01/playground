ddb36818e7ef33b099d2d0f197b452e83fb4194f
krafsan01 <72938634+krafsan01@users.noreply.github.com>
2026-07-08 08:45:25 +0600

    Add files via upload

---
+++ week-8 Golang/day-1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var name string = "rafsan"
+	var age int = 25
+
+	name := "tanvir"
+
+	a := true
+	a = false
+	a = true
+
+	fmt.Println(age)
+	fmt.Println(a)
+	fmt.Println(name)
+	fmt.Println(1 + 1)
+	fmt.Println(true)
+	fmt.Println(false)
+	fmt.Println(10.55566)
+}
---
+++ week-8 Golang/day-1/reply-tool.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	i := "hello"
+
+	switch i {
+	case "hello":
+		fmt.Println("hello Rafsan")
+	}
+}
